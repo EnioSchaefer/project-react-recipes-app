672e5f55192c76bb18aeb765918486e1272bddc9
Karla Oshikawa <karla_hara@hotmail.com>
2022-12-01 08:45:53 +0900

    wip-Requisito 25-1

--- src/service/fetchData.js
+++ src/service/fetchData.js
@@ -1,12 +1,12 @@
-const fetchData = (id, meal) => {
-  const url = meal ? `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+const fetchData = async (id, meal) => {
+  const url = meal ? `www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
     : `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
-  const request = fetch(url)
-    .then((response) => response.json())
-    .then((data) => data[meal ? 'meals' : 'drinks'])
-    .catch((error) => console.error(error));
-  console.log(request);
-  return request;
+  const request = await fetch(url);
+  const response = await request.json();
+  // ((data) => data[meal ? 'meals' : 'drinks'])
+  // catch((error) => console.error(error));
+  console.log(response[meal ? 'meals' : 'drinks']);
+  return response[meal ? 'meals' : 'drinks'];
 };
 
 export default fetchData;
