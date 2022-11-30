ea30d1fe53b2f68bcc13ceb71614e4d3f2eb1343
Victor Hugo <victorhugo-correia@hotmail.com>
2022-11-30 19:06:22 -0300

    [Feat] :rocket: Requisito 9 concluido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
 import { PropTypes } from 'prop-types';
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 import searchIconSVG from '../images/searchIcon.svg';
 import profileIconSVG from '../images/profileIcon.svg';
 
 export default function Header({ title, showSearch }) {
   const history = useHistory();
+  const [showInput, setShowInput] = useState(false);
 
   const toProfile = () => {
     history.push('/profile');
+  };
+
+  const showAndHideInput = () => {
+    if (showInput === false) {
+      setShowInput(true);
+    } if (showInput === true) {
+      setShowInput(false);
+    }
   };
 
   return (
@@ -22,9 +32,16 @@ export default function Header({ title, showSearch }) {
           alt="SVG profile"
         />
       </button>
+      {showInput && (
+        <input
+          data-testid="search-input"
+          type="text"
+        />
+      )}
       {showSearch && (
         <button
           type="button"
+          onClick={ showAndHideInput }
           data-testid="search-button"
         >
           <img
