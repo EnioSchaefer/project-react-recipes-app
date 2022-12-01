145bd3ca628564e3ca86775d7cfa004f0275812a
Enio Schaefer <enio100schaefer@hotmail.com>
2022-12-01 17:14:58 -0300

    [Fix] :bug: remove estado local nao utilizado

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -14,7 +14,6 @@ function Recipes() {
   const dataOf = meal ? 'meals' : 'drinks';
   const idOf = meal ? 'idMeal' : 'idDrink';
   const [apiResponse, setApiResponse] = useState(null);
-  const [categories, setCategories] = useState(null);
   const { filterCategory } = useContext(RecipeContext);
 
   useEffect(() => {
