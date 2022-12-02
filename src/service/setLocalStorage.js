c308bfcbd04f2fdf4a066cf8f29bdbba88ef0104
Enio Schaefer <enio100schaefer@hotmail.com>
2022-12-02 18:22:40 -0300

    [Feat] :rocket: Adiciona a funcionalidade de remover dos favoritos

--- src/service/setLocalStorage.js
+++ src/service/setLocalStorage.js
@@ -1,14 +1,20 @@
 const setLocalStorage = (recipe, isMeal) => {
   const id = recipe[isMeal ? 'idMeal' : 'idDrink'];
+
+  const localStg = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  const currLocalStg = localStg || [];
+  const duplicate = currLocalStg.find((locRecipe) => locRecipe.id === id);
+  if (duplicate) {
+    const removed = currLocalStg.filter((locRecipe) => locRecipe.id !== id);
+    return localStorage.setItem('favoriteRecipes', JSON.stringify(removed));
+  }
+
   const nationality = isMeal ? recipe.strArea : '';
   const type = isMeal ? 'meal' : 'drink';
   const category = recipe.strCategory;
   const alcoholicOrNot = isMeal ? '' : recipe.strAlcoholic;
   const name = recipe[isMeal ? 'strMeal' : 'strDrink'];
   const image = recipe[isMeal ? 'strMealThumb' : 'strDrinkThumb'];
-
-  const localStg = JSON.parse(localStorage.getItem('favoriteRecipes'));
-  const currLocalStr = localStg || [];
 
   const newRecipe = {
     id,
@@ -20,7 +26,7 @@ const setLocalStorage = (recipe, isMeal) => {
     image,
   };
 
-  localStorage.setItem('favoriteRecipes', JSON.stringify([...currLocalStr, newRecipe]));
+  localStorage.setItem('favoriteRecipes', JSON.stringify([...currLocalStg, newRecipe]));
 };
 
 export default setLocalStorage;
