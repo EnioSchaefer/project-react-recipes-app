dd27ce42a8044723eba6dbea3c1d01979f8e305e
Hércules Araujo <hrclsrj@gmail.com>
2022-12-06 16:43:52 -0300

    [Feat] :rocket: Tela de Login/ Req 2,3 e 5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Login from './components/Login';
+import TelaPrincipal from './components/TelaPrincipal';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
+
+    <div>
+      {/* <span className="logo">TRYBE</span>
       <object
         className="rocksGlass"
         type="image/svg+xml"
         data={ rockGlass }
       >
         Glass
-      </object>
+      </object> */}
+      {/* <Login /> */}
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/meals" component={ TelaPrincipal } />
+        <Route exact path="/drinks" component={ TelaPrincipal } />
+        {/* falta especificar o componente certo da tela Priincipal */}
+      </Switch>
     </div>
   );
 }
