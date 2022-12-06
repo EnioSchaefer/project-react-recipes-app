f192b1dc896410cede2e09d22057cad3818f93e7
Enio Schaefer <enio100schaefer@hotmail.com>
2022-12-06 18:07:56 -0300

    [Fix] :bug: Remove context nao utilizado e problemas de importacao no App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import RecipeDetails from './components/RecipeDetails';
 import MainRecipes from './pages/MainRecipes';
-
 import Login from './components/Login';
-import TelaPrincipal from './components/TelaPrincipal';
 
 function App() {
   return (
     <Switch>
+      <Route exact path="/" component={ Login } />
       <Route exact path="/drinks" component={ MainRecipes } />
       <Route exact path="/meals" component={ MainRecipes } />
       <Route exact path="/drinks/:id" component={ RecipeDetails } />
       <Route exact path="/meals/:id" component={ RecipeDetails } />
-      <Route exact path="/done-recipes" component={ DoneRecipes } />
+      {/* <Route exact path="/done-recipes" component={ DoneRecipes } /> */}
     </Switch>
   );
 }
