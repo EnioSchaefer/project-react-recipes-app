1698ea16150f4fad02db5506b1a1dc55f30e56a9
Karla Oshikawa <karla_hara@hotmail.com>
2022-12-07 10:53:15 +0900

    feat :rocket: requisito 44, 45

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -6,6 +6,7 @@ import shareIcon from '../images/shareIcon.svg';
 export default function DoneRecipes() {
   const { isMeal } = useContext(RecipeContext);
   const doneRecipesList = JSON.parse(localStorage.getItem('doneRecipes') || '[]');
+  console.log(doneRecipesList[0].name);
 
   return (
     <div>
@@ -31,24 +32,35 @@ export default function DoneRecipes() {
         doneRecipesList.map((item, index) => (
           <div key={ index }>
             <img src={ item.image } alt="" data-testid={ `${index}-horizontal-image` } />
-            <p data-testid={ `${index}-horizontal-top-text` }>{item.category}</p>
             <p data-testid={ `${index}-horizontal-name` }>{item.name}</p>
             <p data-testid={ `${index}-horizontal-done-date` }>{item.doneDate}</p>
-            {isMeal && (
-              <p data-testid={ `${index}-horizontal-top-text` }>
-                {`${item.nationality} - ${item.category}`}
-              </p>)}
+            {item.type === 'meal' ? (
+              <div>
+                <p data-testid={ `${index}-horizontal-top-text` }>
+                  {`${item.nationality} - ${item.category}`}
+                </p>
+                <p data-testid={ `${index}-${item.tags[0]}-horizontal-tag` }>
+                  {item.tags[0]}
+                </p>
+                <p data-testid={ `${index}-${item.tags[1]}-horizontal-tag` }>
+                  {item.tags[1]}
+                </p>
+              </div>
+            )
+              : (
+                <p data-testid={ `${index}-horizontal-top-text` }>
+                  {item.alcoholicOrNot}
+                </p>
+              )}
             <button
               type="button"
-              // onClick={ shareLink }
-              data-testid={ `${index}-horizontal-share-btn` }
             >
               <img
+                data-testid={ `${index}-horizontal-share-btn` }
                 src={ shareIcon }
                 alt="share button"
               />
             </button>
-            <p data-testid={ `${index}-${item.tags}-horizontal-tag` }>{item.tags}</p>
           </div>
         ))
       }
