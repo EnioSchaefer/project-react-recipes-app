1e209e6f16c3c9b5eac7def01b40f483053e4b35
Victor Hugo <victorhugo-correia@hotmail.com>
2023-01-11 20:36:30 -0300

    [Fix] corrigindo lint

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import Header from '../components/Header';
+import { useEffect, useState } from 'react';
 import shareIcon from '../images/shareIcon.svg';
+import Header from '../components/Header';
 import handleClick from '../service/setDoneLink';
 import './DoneRecipes.css';
 
@@ -9,18 +9,15 @@ export default function DoneRecipes() {
   const [showCopyMessage, setShowCopyMessage] = useState(false);
   const [doneRecipesList, setDoneRecipesList] = useState([]);
   const [doneList, setDoneList] = useState(null);
-
   useEffect(() => {
     const list = JSON.parse(localStorage.getItem('doneRecipes'));
     setDoneRecipesList(list);
     setDoneList(list);
   }, []);
-
   const handleFilter = ({ target }) => {
     const filter = doneRecipesList.filter((el) => target.id === el.type);
     setDoneRecipesList(filter);
   };
-
   return (
     <div>
       <Header title="Done Recipes" showSearch={ false } />
@@ -32,7 +29,6 @@ export default function DoneRecipes() {
       >
         All
       </button>
-
       <button
         type="button"
         data-testid="filter-by-meal-btn"
@@ -41,7 +37,6 @@ export default function DoneRecipes() {
       >
         Meals
       </button>
-
       <button
         type="button"
         data-testid="filter-by-drink-btn"
@@ -62,71 +57,6 @@ export default function DoneRecipes() {
               />
             </Link>
             <Link to={ `${item.type}s/${item.id}` }>
-              <p data-testid={ `${index}-horizontal-name` }>{item.name}</p>
-            </Link>
-            <p data-testid={ `${index}-horizontal-done-date` }>{item.doneDate}</p>
-            {item.type === 'meal' ? (
-              <div>
-                <p data-testid={ `${index}-horizontal-top-text` }>
-                  {`${item.nationality} - ${item.category}`}
-                </p>
-                <p data-testid={ `${index}-${item.tags[0]}-horizontal-tag` }>
-                  {item.tags[0]}
-                </p>
-                <p data-testid={ `${index}-${item.tags[1]}-horizontal-tag` }>
-                  {item.tags[1]}
-                </p>
-              </div>
-            )
-              : (
-                <p data-testid={ `${index}-horizontal-top-text` }>
-                  {item.alcoholicOrNot}
-                </p>
-              )}
-            <button
-              type="button"
-              onClick={ ({ target }) => {
-                handleClick({ target }, setShowCopyMessage);
-              } }
-            >
-              <img
-                data-testid={ `${index}-horizontal-share-btn` }
-                src={ shareIcon }
-                alt="share button"
-                name={ item.type }
-                id={ item.id }
-              />
-            </button>
-            {showCopyMessage
-              && <p style={ { fontSize: '10px' } } data-testid="share">Link copied!</p>}
-          </div>
-        ))
-      }
-    </div>
-  );
-}
-
-// import Header from '../components/Header';
-
-      <button
-        type="button"
-        data-testid="filter-by-drink-btn"
-        id="drink"
-        onClick={ ({ target }) => handleFilter({ target }) }
-      >
-        Drinks
-      </button>
-      {
-        doneRecipesList && doneRecipesList.map((item, index) => (
-          <div key={ index } data-testid="done">
-            <Link
-              to={ `${item.type}s/${item.id}` }
-            >
-              <img
-                src={ item.image }
-                alt=""
-                data-testid={ `${index}-horizontal-image` }
-              />
               <p data-testid={ `${index}-horizontal-name` }>{item.name}</p>
             </Link>
             <p data-testid={ `${index}-horizontal-done-date` }>{item.doneDate}</p>
--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -49,4 +49,89 @@ describe('Testes do SearchBar', () => {
 
     userEvent.click(filterSearchButton);
   });
+
+  it('Verifica se todos os botões e inputs são passíveis de clique ou escrita', async () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => history.push('/meals'));
+    await screen.findAllByText('Meals');
+
+    const searchButton = await screen.findByTestId(searchBtn);
+    userEvent.click(searchButton);
+
+    const searchInput = await screen.findByTestId(searchInpt);
+    expect(searchInput).toBeVisible();
+    userEvent.type(searchInput, 'oil');
+
+    const ingredientRadio = await screen.findByTestId('ingredient-search-radio');
+    expect(ingredientRadio).toBeInTheDocument();
+    userEvent.click(ingredientRadio);
+
+    const filterSearchButton = await screen.findByTestId(execSearch);
+    userEvent.click(filterSearchButton);
+  });
+
+  it('Verifica se ao digitar mais de uma letra na busca por first letter é exibido um aviso/alert', async () => {
+    renderWithRouter(<App />);
+    window.alert = () => {};
+
+    const searchButton = await screen.findByTestId(searchBtn);
+    userEvent.click(searchButton);
+
+    const firstLetterRadio = await screen.findByTestId('first-letter-search-radio');
+    userEvent.click(firstLetterRadio);
+
+    const searchInput = await screen.findByTestId(searchInpt);
+    expect(searchInput).toBeVisible();
+    userEvent.type(searchInput, 'chicken');
+
+    const filterSearchButton = await screen.findByTestId(execSearch);
+    userEvent.click(filterSearchButton);
+  });
+
+  it('Verifica se o resultado da pesquisa tiver apenas um resultado a tela é redirecionada para pagina de detalhes', async () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/drinks');
+    });
+
+    const searchButton = await screen.findByTestId(searchBtn);
+    userEvent.click(searchButton);
+
+    const nameRadio = await screen.findByTestId(nameSearch);
+    userEvent.click(nameRadio);
+
+    const searchInput = await screen.findByTestId(searchInpt);
+    userEvent.type(searchInput, 'ABC');
+
+    const filterSearchButton = await screen.findByTestId(execSearch);
+    userEvent.click(filterSearchButton);
+
+    await waitFor(() => {
+      expect(history.location.pathname).toBe('/drinks/13501');
+    });
+  });
+
+  it('Verifica se a pesquisa é inexistente', async () => {
+    const { history } = renderWithRouter(<App />);
+
+    window.alert = () => {};
+
+    act(() => {
+      history.push('/meals');
+    });
+
+    const searchButton = await screen.findByTestId(searchBtn);
+    userEvent.click(searchButton);
+
+    const nameRadio = await screen.findByTestId(nameSearch);
+    userEvent.click(nameRadio);
+
+    const searchInput = await screen.findByTestId(searchInpt);
+    userEvent.type(searchInput, 'arroz');
+
+    const filterSearchButton = await screen.findByTestId(execSearch);
+    userEvent.click(filterSearchButton);
+  });
 });
